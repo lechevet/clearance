481ee1436f4a3b5904e5de83de3451fdc14791f2
loïc <loic@loic-ThinkPad-T61.(none)>
2013-09-30 15:40:52 +0400

    add confirmation methods

--- app/mailers/clearance_mailer.rb
+++ app/mailers/clearance_mailer.rb
@@ -6,4 +6,12 @@ class ClearanceMailer < ActionMailer::Base
          :scope => [:clearance, :models, :clearance_mailer],
          :default => 'Change your password')
   end
+ 
+  def confirmation(user)
+  	@user = user
+  	mail :from => Clearance.configuration.mailer_sender, :to => @user.email,
+  		:subject    I18n.t(:confirmation,
+          :scope   => [:clearance, :models, :clearance_mailer],
+          :default => "Account confirmation")
+	end
 end
